4a6d1ccc9cff7fceddd681e82451fb7393287801
tdq <tao_duoqi@163.com>
2020-07-18 17:44:36 +0800

    优化

--- app/src/main/java/com/example/coroutinesdemo/ui/navigation/Blank1Fragment.kt
+++ app/src/main/java/com/example/coroutinesdemo/ui/navigation/Blank1Fragment.kt
@@ -1,8 +1,9 @@
 package com.example.coroutinesdemo.ui.navigation
 
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
-import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment.findNavController
 import com.example.coroutinesdemo.R
 import com.example.coroutinesdemo.base.BaseVMFragment
 import kotlinx.android.synthetic.main.fragment_blank1.*
@@ -13,11 +14,17 @@ class Blank1Fragment : BaseVMFragment(useDataBinding = false) {
 
     override fun initView() {
         tvBlank1.setOnClickListener {
-            Navigation.findNavController(it).navigate(R.id.blank2Fragment, Bundle().apply {
+            findNavController(this).navigate(R.id.blank2Fragment, Bundle().apply {
                 putString("param1", "222")
             })
         }
+
         Log.e("Blank1Fragment", "initView")
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        Log.e("Blank1Fragment", "onAttach")
     }
 
     override fun startObserve() {
